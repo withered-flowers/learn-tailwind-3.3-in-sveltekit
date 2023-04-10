53b75e5455d8cef764d7700ca1843ecfe36b70c1
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-04-10 20:06:55 +0700

    feat: adding content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,52 @@
+<script lang="ts">
+	enum Direction {
+		RTL = 'rtl',
+		LTR = 'ltr'
+	}
+
+	let toggleDirection = Direction.RTL;
+
+	const onClickHandler = () => {
+		toggleDirection = toggleDirection === Direction.RTL ? Direction.LTR : Direction.RTL;
+		console.log(toggleDirection);
+	};
+</script>
+
 <h1 class="text-3xl font-semibold underline">Hello Tailwind v3.3!</h1>
 
 <main class="p-4">
 	<h3 class="text-xl/loose font-semibold text-gray-700/90">Extended Color Palette</h3>
+	<section class="p-4 bg-slate-700 rounded md:w-1/2 flex items-center justify-center">
+		<input
+			type="text"
+			placeholder="An input with slate-950"
+			class="rounded py-2 px-2 w-full bg-slate-950 text-white placeholder:text-white"
+		/>
+	</section>
+</main>
+
+<main class="p-4">
+	<h3 class="text-xl/loose font-semibold text-gray-700/90">Easier LTR and RTL</h3>
+	<section class="group flex items-center py-4 {toggleDirection}">
+		<img
+			class="shrink-0 h-12 w-12 rounded-full"
+			src="https://placekitten.com/48/48"
+			alt="a kitten"
+		/>
+		<div class="ms-3 transition-all duration-1000">
+			<p class="text-sm font-medium text-slate-700 group-hover:text-slate-900">
+				Now we can use ms-* (margin-start) and me-* (margin-end)
+			</p>
+			<p class="text-sm font-medium text-slate-500 group-hover:text-slate-700">
+				Bye ltr:ml-* and rtl:mr-*
+			</p>
+		</div>
+	</section>
+	<button
+		on:click={onClickHandler}
+		class="bg-emerald-300 py-2 px-4 rounded hover:bg-emerald-500 hover:text-slate-100 transition-colors duration-300"
+		>Click to Change Direction</button
+	>
 </main>
 
 <main class="p-4 md:w-1/2 lg:w-1/3 w-full">
@@ -53,5 +98,13 @@
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
+	}
+
+	.ltr {
+		direction: ltr;
+	}
+
+	.rtl {
+		direction: rtl;
 	}
 </style>
